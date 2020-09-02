9c4bbf86fed76e36884783736fe1bfe4fe904a11
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-09-02 13:05:27 -0500

    eslint fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Display } from './Display';
-import { ButtonPanel } from './ButtonPanel';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
-export const App = () => {
-    return (
-      <div id='app'>
-        <Display result=''/>
-        <ButtonPanel />
-      </div>
-    );
-}
+const App = () => (
+  <div id="app">
+    <Display result="" />
+    <ButtonPanel />
+  </div>
+);
+
+export default App;
